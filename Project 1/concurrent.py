aa49c1a4796bd4d952880e039ba742a9a6f55325
just331 <justinr331@gmail.com>
2020-03-30 00:16:12 -0500

    Added Concurret run of programs

--- Project 1/concurrent.py
+++ Project 1/concurrent.py
@@ -4,15 +4,7 @@ import Project1 as p1
 import Project1Chaining as p2
 
 
-def main():
-    t1 = Thread(target=p1)
-    t2 = Thread(target=p2)
+if __name__ == '__main__':
+    p1
+    p2
 
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-
-main()
